8a0465ff481170a4dabee6259407eaeee63c1d67
Alex B <ajbarbati@gmail.com>
2019-07-27 19:17:32 -0400

    added buyIt function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,6 +16,63 @@ connection.connect(function(err) {
     console.log('ID: ' + connection.threadId)
 })
 
+function buyIt() {
+
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'item_id',
+			message: 'Please enter the Item ID which you would like to purchase.',
+			validate: validateInput,
+			filter: Number
+		},
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many do you need?',
+			validate: validateInput,
+			filter: Number
+		}
+	]).then(function(input) {
+
+		var item = input.item_id
+		var quantity = input.quantity
+
+		var queryStr = 'SELECT * FROM products WHERE ?'
+
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err
+
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+				allProducts()
+
+			} else {
+				var productData = data[0]
+
+				if (quantity <= productData.stock_quantity) {
+					console.log('Congratulations, the product you requested is in stock! Placing order!')
+					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err
+
+						console.log('Your oder has been placed! Your total is $' + productData.price * quantity)
+						console.log('Thank you for shopping with us!')
+						console.log("\n---------------------------------------------------------------------\n")
+						connection.end()
+					})
+				} else {
+					console.log('Sorry, there is not enough product in stock, your order can not be placed as is.')
+					console.log('Please modify your order.')
+					console.log("\n---------------------------------------------------------------------\n")
+
+					allProducts()
+				}
+			}
+		})
+	})
+}
+
 function allProducts() {
 	queryStr = 'SELECT * FROM products'
 
@@ -46,18 +103,6 @@ allProducts()
   connection.forEach( (row) => {
     console.log(`${row.product_name} is in ${row.department_name}`)
 })
-
-function buyProduct () {
-    console.log('What is the ID of the product you would like to purchase?')
-    
-    console.log('How many units would you like?')
-}
-function quantityCheck () {
-    if (quantity === 0) {
-        console.log('All out!')
-        allProducts()
-    } else checkOut()
-}
 
 function checkOut () {
     console.log("Grabbing your product from the back");
